457f2948b5eea7e374085e7c216eb10d02932eb3
sarlango <fer@a.com>
2018-03-30 16:27:14 -0300

    a ver ahi

--- src/main/java/com/farguito/online/Mundo.java
+++ src/main/java/com/farguito/online/Mundo.java
@@ -8,7 +8,6 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
-import org.junit.runner.notification.RunListener.ThreadSafe;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
