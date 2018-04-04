b9d4f45c5b39fd641a490e310589e003470eb9c6
muehehehe <fer@desde-el-laburo.com>
2018-04-04 11:41:27 -0300

    se me escapó la tortuga

--- src/main/java/com/farguito/JotasonDungeonApplication.java
+++ src/main/java/com/farguito/JotasonDungeonApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JotasonDungeonApplication {
 
-	//public static String URL = "https://jotason-dungeon.herokuapp.com";
-	public static String URL = "http://localhost:8080";
+	public static String URL = "https://jotason-dungeon.herokuapp.com";
+	//public static String URL = "http://localhost:8080";
 	
 	public static void main(String[] args) {
 		SpringApplication.run(JotasonDungeonApplication.class, args);
